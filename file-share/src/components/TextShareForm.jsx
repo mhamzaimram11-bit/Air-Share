a203cf8fdade8115d72983b8f49c55eb0dffdcec
hamza <m.hamzaimram11@gmail.com>
2025-11-19 16:58:52 +0500

    fix: fixes in server & upload form

--- file-share/src/components/TextShareForm.jsx
+++ file-share/src/components/TextShareForm.jsx
@@ -9,7 +9,6 @@ export default function TextShareForm() {
 
   useEffect(() => {
     const registerIP = async () => {
-      //  4220171470471
       try {
         const res = await fetch("https://api64.ipify.org?format=json");
         const data = await res.json();
